cd0c5541f0e3f345e38d3c2ea2d5e58fd9c67478
passwd10 <parkinseo94@naver.com>
2020-02-08 01:12:04 +0900

    Add google site vertification

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import Top from '../top'
 import Footer from '../footer'
+import Helmet from 'react-helmet'
 
 import './index.scss'
 
 export const Layout = ({ children, title }) => (
   <>
+    <Helmet>
+      <meta 
+        name="google-site-verification"
+        content="PbckaZEG4R6mAQ8vefqPLmYbaftIqcI9Bi-VJ1Fj2so" />
+    </Helmet>
     <Top title={title} />
     <div className='layout-body'>
       {children}
